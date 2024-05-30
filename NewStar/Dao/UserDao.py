40dda4cc44f9d7fa19cc35c1914698dc3c4f3a18
JiaWei-Chen-2295 <1601020332@qq.com>
2024-05-30 13:01:49 +0800

    修改UserDao对象

--- NewStar/Dao/UserDao.py
+++ NewStar/Dao/UserDao.py
@@ -1,7 +1,7 @@
 """
 :description: 操作数据库中 User 表的类
 :author: JavierChen
-:version: 1.0
+:version: 2.0
 """
 from NewStar.Dao.BaseDao import BaseDao
 from NewStar.Objects.User import User
@@ -9,105 +9,103 @@ from NewStar.Objects.User import User
 
 class UserDao():
     """
-    数据库 Students 表操作类，用于管理 Students 表中的学生记录。
+        数据库 Users 表操作类，用于管理 Users 表中的用户记录。
 
-    Methods:
-        insert: 向 Students 表插入一个新的学生记录。
-        update: 根据学生ID更新 Students 表中已有学生记录。
-        delete: 根据学生ID从 Students 表中删除学生记录。
-        selectById: 根据学生ID查询 Students 表中的学生记录。
-        selectAll: 查询 Students 表中的所有学生记录。
+        Methods:
+            insert: 向 Users 表插入一个新的用户记录。
+            update: 根据用户ID更新 Users 表中已有用户记录。
+            delete: 根据用户ID从 Users 表中删除用户记录。
+            selectById: 根据用户ID查询 Users 表中的用户记录。
+            selectAll: 查询 Users 表中的所有用户记录。
     """
 
     def insert(self, User):
         """
-        向 Students 表插入一条新的学生记录。
+                向 Users 表插入一条新的用户记录。
 
-        参数:
-            - student: `Student` 类的实例，包含学生的学号、姓名、一卡通号及班级ID。
+                参数:
+                    - user: `User` 类的实例，包含用户的用户名、密码、角色、班级ID及学生ID。
 
-        异常:
-            - 抛出异常，当插入操作失败时。
-        """
+                异常:
+                    - 抛出异常，当插入操作失败时。
+         """
         sql = "INSERT INTO Users (username, password, role, class_id, student_id) VALUES (?, ?, ?, ?, ?)"
         r = BaseDao.executeUpdate(sql, User.username, User.password, User.role, User.class_id, User.student_id)
         if r == -1:
             raise Exception("Insert operation failed.")
 
-    def update(self, student):
+    def update(self, User):
         """
-        根据学生ID更新 Students 表中已有的学生记录。
+                根据用户ID更新 Users 表中已有的用户记录。
 
-        参数:
-            - student: `Student` 类的实例，需包含学生ID及要更新的其他信息。
+                参数:
+                    - user: `User` 类的实例，需包含用户ID及要更新的其他信息。
 
-        异常:
-            - 抛出异常，当更新操作失败时。
-        """
-        sql = "UPDATE Students SET username = ?, password = ?, role = ?, class_id = ?, student_id WHERE user_id = ?"
-        r = BaseDao.executeUpdate(sql, User.username, User.password, User.role, )
+                异常:
+                    - 抛出异常，当更新操作失败时。
+                """
+        sql = f"UPDATE Users SET username = ?, password = ?, role = ?, class_id = ?, student_id = ? WHERE user_id = ?"
+        r = BaseDao.executeUpdate(sql, User.username, User.password, User.role, User.class_id, User.student_id, User.user_id)
         if r == -1:
             raise Exception("Update operation failed.")
 
-    def drop(self, student):
+    def drop(self, User):
         """
-        根据学生ID从 Students 表中删除学生记录。
+                根据用户ID从 Users 表中删除用户记录。
 
-        参数:
-            - student: `Student` 类的实例，至少需要包含学生ID。
+                参数:
+                    - user: `User` 类的实例，至少需要包含用户ID。
 
-        异常:
-            - 抛出异常，当删除操作失败时。
+                异常:
+                    - 抛出异常，当删除操作失败时。
         """
-        sql = "DELETE FROM Students WHERE student_id = ?"
-        r = BaseDao.executeUpdate(sql, student.id)
+        sql = "DELETE FROM Users WHERE user_id = ?"
+        r = BaseDao.executeUpdate(sql, User.user_id)
         if r == -1:
             raise Exception("Delete operation failed.")
 
     def selectById(self, id):
         """
-        根据学生ID查询 Students 表中的学生记录。
+        根据用户ID查询 Users 表中的用户记录。
 
         参数:
-            - id: 要查询的学生ID。
+            - user_id: 要查询的用户ID。
 
         返回:
-            - 查询到的 `Student` 实例，如果没有找到则返回 None 或抛出异常（根据需求选择实现）。
+            - 查询到的 `User` 实例，如果没有找到则返回 None 或抛出异常（根据需求选择实现）。
         """
-        sd = StudentDao()
-        rs = sd.selectAll()
+        ud = UserDao()
+        rs = ud.selectAll()
         for _ in rs:
-            if _.id == id:
+            if _.user_id == id:
                 return _
 
     def selectAll(self):
         """
-        查询 Students 表中的所有学生记录。
+        查询 Users 表中的所有用户记录。
 
         返回:
-            - 包含所有学生记录的 `Student` 实例列表。
+            - 包含所有用户记录的 `User` 实例列表。
         """
-        student_list = []
-        sql = "SELECT * FROM Students"
+        user_list = []
+        sql = "SELECT * FROM Users"
         bd = BaseDao()
         rs = bd.executeQuery(sql)
         for row in rs:
-            student = Student(row[1], row[2], row[3], row[0])
-            student_list.append(student)
-        return student_list
+            user = User(row[1], row[2], row[3], row[4], row[5], row[0])
+            user_list.append(user)
+        return user_list
 
 if __name__ == '__main__':
-    m = Student("fht4", 222222222222221166, 2, 7)
-    md = StudentDao()
+    m = User(None,None,'admin',id=10)
+    md = UserDao()
     md.drop(m)
     # 示例代码可以在这里编写，用于测试 StudentDao 类的各种方法
-    # md = StudentDao()
+    # md = UserDao()
     # rs = md.selectById(2)
     # print(rs)
 
-    # m = Student("fht4", 222222222222221166, 2)
-    # md = StudentDao()
-    # md.insert(m)
-    #
+    # m = User("fht4", '2a1166', 'admin',1,1,9)
+    # md = UserDao()
+    # md.update(m)
 
-    #
