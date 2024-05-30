01dc1e66570f17918fd519d6be8c45d555a97c1d
Franklin <14469247+franklintyr@user.noreply.gitee.com>
2024-05-30 13:01:33 +0800

    admin view 权限实现

--- NewStar/Manager/function.py
+++ NewStar/Manager/function.py
@@ -1,3 +1,4 @@
+from NewStar.Objects.User import User
 def add():
     from NewStar.Objects.User import User
     id = input('请输入id')
@@ -8,19 +9,18 @@ def add():
     student_id = input('请输入学生id')
     user = User(id_name,password,id_role,Class_id,student_id,id)
     print(user)
-# def view_information():
-#     print('-'*60)
-#     print('学生姓名\t\t学生身份证号\t\t学生性别\t\t学生年龄\t\t学生学号\t\t学生班级\t\t学生专业')
-#         id_1 = information['id']
-#         idcard_1 = information['idcard']
-#         gender_1 = information['gender']
-#         age_1 = information['age']
-#         id_study_1 = information['id_study']
-#         Class_1 = information['Class']
-#         profession_1 = information['profession']
-#         information_1 = '%s\t\t\t\t%s\t\t\t\t%s\t\t\t\t%s\t\t\t\t%s\t\t\t\t%s\t\t\t%s\t\t\t' % (id_1,idcard_1,gender_1,age_1,
-#                                                                                                 id_study_1,Class_1,profession_1)
-#         print(information_1)
-#
+def view_all_information(user):
+    if User.role == 'admin':
+        print('学生姓名\t\t学生身份证号\t\t学生性别\t\t学生年龄\t\t学生学号\t\t学生班级\t\t学生专业')
+        id_1 = User.user_id
+        id_name_1 = User.username
+        password_1 = User.password
+        id_role_1 = User.role
+        Class_id_1 = User.class_id
+        student_id_1 = User.student_id
+        information_1 = '%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t' % (id_1,id_name_1,password_1,id_role_1,
+                                                                  Class_id_1,student_id_1)
+        print(information_1)
+
 if __name__ == '__main__':
     add()
